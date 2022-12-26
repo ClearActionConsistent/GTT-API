1cd2d5c9dfcd78a29d4c6cad301cb60233b8369d
sdangTMA <sdang@orangetheory.com>
2022-12-26 10:16:45 +0700

    feat/95 [Admin Side - API] Add Excercise Library Mangament

---
+++ src/Services/GTT/shared/GTT.Application/Commands/CreateExerciseLib.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using GTT.Application.Interfaces.Repositories;
+using GTT.Application.Requests.ExerciseLib;
+using GTT.Application.Response;
+using MediatR;
+
+namespace GTT.Application.Commands.ExerciseLibrary
+{
+    public class CreateExerciseLib
+    {
+        public record Command(CreateExerciseLibRequestModel data) : IRequest<BaseResponseModel>;
+
+        public class Valiator : AbstractValidator<Command>
+        {
+            public Valiator()
+            {
+                RuleFor(x => x.data.ExerciseName)
+                    .NotEmpty().WithMessage("Exercise name is not empty")
+                    .NotNull().WithMessage("Exercise name is not null");
+                RuleFor(x => x.data.ClassId)
+                    .NotEmpty().WithMessage("Class id is not empty")
+                    .NotNull().WithMessage("Class id is not null")
+                    .GreaterThan(0).WithMessage("Class id is greater than 0");
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, BaseResponseModel>
+        {
+            private readonly  IExerciseLibRepository _exerciseLibRepository;
+
+            public Handler(IExerciseLibRepository exerciseLibRepository)
+            {
+                _exerciseLibRepository = exerciseLibRepository;
+            }
+
+            public async Task<BaseResponseModel> Handle(Command request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var result = await _exerciseLibRepository.CreateExerciseLib(request.data);
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    var error = $"[Handle] CreateExGroup - {Helpers.BuildErrorMessage(ex)}";
+                    throw new Exception(error);
+                }
+            }
+        }
+    }
+}
