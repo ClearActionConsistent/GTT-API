75a614a569607e51f7613f5dd96ae259bb4dd127
phantuananh18 <tuananh181120@gmail.com>
2023-01-03 15:55:35 +0700

    feat/104-Get-Ex-Group: Remove extension not use

--- src/Services/GTT/shared/GTT.Application/Extensions/GTTPagingUtility.cs
+++ src/Services/GTT/shared/GTT.Application/Extensions/GTTPagingUtility.cs
@@ -2,34 +2,6 @@
 {
     public class GTTPagingUtility
     {
-        public static GTTPageResults<T> CreatePagedResults<T>(
-            IEnumerable<T> queryable,
-            int page,
-            int pageSize)
-        {
-            var skipAmount = pageSize * (page - 1);
-
-            var enumerable = queryable as IList<T> ?? queryable?.ToList();
-            var projection = enumerable?
-                .Skip(skipAmount)
-                .Take(pageSize);
-
-            var totalNumberOfRecords = enumerable?.Count ?? 0;
-            var results = projection?.ToList();
-
-            var mod = totalNumberOfRecords % pageSize;
-            var totalPageCount = (totalNumberOfRecords / pageSize) + (mod == 0 ? 0 : 1);
-
-            return new GTTPageResults<T>
-            {
-                Results = results,
-                PageNumber = page,
-                PageSize = pageSize,
-                TotalNumberOfPages = totalPageCount,
-                TotalNumberOfRecords = totalNumberOfRecords
-            };
-        }
-
         public static GTTPageResults<T> CreatePagedResultsQuery<T>(
             IEnumerable<T> results,
             int page,
