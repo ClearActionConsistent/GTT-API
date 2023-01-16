8d0b4c5ad9fe7e605bb5341b49bb88ead55da292
Duc Vo <duclionel0702@gmail.com>
2023-01-16 14:34:23 +0700

    [GREAT-56]: Create and Build database SQL project

---
+++ GTT.Database/dbo/Tables/Exercises.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Exercises] (
+    [ExerciseId]   INT            IDENTITY (1, 1) NOT NULL,
+    [ExerciseName] NVARCHAR (255) NULL
+);
+
---
+++ GTT.Database/dbo/Tables/Sports.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Sports] (
+    [SportId]     INT           IDENTITY (1, 1) NOT NULL,
+    [SportImage]  VARCHAR (255) DEFAULT (NULL) NULL,
+    [SportName]   VARCHAR (255) DEFAULT (NULL) NULL,
+    [SportType]   VARCHAR (100) DEFAULT (NULL) NULL,
+    [CreatedBy]   CHAR (36)     DEFAULT (NULL) NULL,
+    [UpdatedBy]   CHAR (36)     DEFAULT (NULL) NULL,
+    [CreatedDate] DATETIME      DEFAULT (getdate()) NOT NULL,
+    [UpdatedDate] DATETIME      DEFAULT (getdate()) NOT NULL,
+    [IsActive]    TINYINT       DEFAULT ('1') NOT NULL,
+    [IsDeleted]   TINYINT       DEFAULT ('0') NOT NULL,
+    PRIMARY KEY CLUSTERED ([SportId] ASC),
+    CHECK ([SportType]='Other Sports' OR [SportType]='Winter Sports' OR [SportType]='Water Sports' OR [SportType]='Cycle Sports' OR [SportType]='Foot Sports')
+);
+
